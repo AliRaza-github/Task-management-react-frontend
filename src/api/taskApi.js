40dfc58b0b43996a33e8b1a7aad45e407e436f51
Ali Raza <123078445+AliRaza-github@users.noreply.github.com>
2024-12-19 17:52:41 +0500

    solve the base url error

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:5000/tasks';
-const API_URL = 'https://task-management-backend-node.vercel.app/tasks/';
+const API_URL = 'https://task-management-backend-node.vercel.app/tasks';
 
 
 
